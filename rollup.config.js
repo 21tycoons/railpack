2f42714bfe6b6ade588f1177268aa6937e5c46ba
Liroy Leshed <liroyleshed@protonmail.com>
2023-11-01 15:25:43 -0700

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  input: "src/index.js",
+  output: {
+    file: "app/javascript/application.js",
+    format: "iife",
+    inlineDynamicImports: true,
+    sourcemap: true
+  },
+}
