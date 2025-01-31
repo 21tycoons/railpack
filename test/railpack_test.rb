36d32bfc2f98a85f66b07c0e7f5b42f2245b3154
Liroy Leshed <liroyleshed@protonmail.com>
2025-01-31 06:48:22 -0800

    Update railpack_test.rb

--- test/railpack_test.rb
+++ test/railpack_test.rb
@@ -10,4 +10,16 @@ class RailpackTest < Minitest::Test
   def test_bun_manager_exists
     assert Railpack::BunManager.new.exists?, true
   end
+
+  def test_webpack_manager_exists
+    assert Railpack::WebpackManager.new.exists?, true
+  end
+
+  def test_rollup_manager_exists
+    assert Railpack::RollupManager.new.exists?, true
+  end
+
+  def test_esbuild_manager_exists
+    assert Railpack::EsbuildManager.new.exists?, true
+  end
 end
