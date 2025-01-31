5ad04bfe4389a951431e962a5e9fb43f2dc0dedc
Liroy Leshed <liroyleshed@protonmail.com>
2025-01-31 06:33:24 -0800

    Update railpack.rb

--- lib/railpack.rb
+++ lib/railpack.rb
@@ -9,3 +9,11 @@ module Railpack
     # Your code goes here...
   end
 end
+
+
+require "./railpack/instance"
+require "./railpack/configuration"
+require "./railpack/bun_manager"
+require "./railpack/webpack_manager"
+require "./railpack/esbuild_manager"
+require "./railpack/rollup_manager"
